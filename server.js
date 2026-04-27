7f2db46e53ada53351f16123563d8431f5e3cfe7
Chris Calver <chriscalver@hotmail.com>
2026-04-27 09:19:02 -0400

    cors

--- server.js
+++ server.js
@@ -35,7 +35,8 @@ try {
     "http://localhost:8080",
     "http://localhost:8081",
     "http://127.0.0.1:8080",
-    "http://127.0.0.1:8081"
+    "http://127.0.0.1:8081",
+    "http://127.0.0.1:5556"
   ];
 
   function isUserNameInUse(name) {
